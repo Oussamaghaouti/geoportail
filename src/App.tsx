3e76bb54124cf448893f75dd3c5ed6af1beccfd2
Oussamaghaouti <oussama.ghaouti0@gmail.com>
2025-02-24 01:15:49 +0000

    'changes'

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Polygon, Popup } from "react-leaflet";
 import {
   Menu,
@@ -9,9 +9,13 @@ import {
   Home,
   ChevronLeft,
   ChevronRight,
+  Moon,
+  Sun,
+  Globe,
 } from "lucide-react";
 import "leaflet/dist/leaflet.css";
 import { LatLngExpression } from "leaflet";
+import { translations, type Language } from "./translations";
 
 // Sample polygons representing different zones
 const polygons: {
@@ -30,7 +34,7 @@ const polygons: {
       ],
     ],
     color: "#4F46E5",
-    name: "Zone Résidentielle A",
+    name: "zoneResidentielleA",
     layer: "couche1",
   },
   {
@@ -43,7 +47,7 @@ const polygons: {
       ],
     ],
     color: "#10B981",
-    name: "Zone Écologique",
+    name: "zoneEcologique",
     layer: "couche1",
   },
   {
@@ -56,7 +60,7 @@ const polygons: {
       ],
     ],
     color: "#F59E0B",
-    name: "Zone Mixte",
+    name: "zoneMixte",
     layer: "couche2",
   },
 ];
@@ -68,26 +72,58 @@ function App() {
     couche1: true,
     couche2: true,
   });
-
+  const [language, setLanguage] = useState<Language>("fr");
   const position: [number, number] = [48.8566, 2.3522]; // Paris coordinates
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   const toggleLayer = (layer: string) => {
     setActiveLayers((prevState) => ({
       ...prevState,
       [layer]: !prevState[layer],
     }));
   };
-
+  const changeLanguage = () => {
+    setLanguage((prevLang) => {
+      switch (prevLang) {
+        case "fr":
+          return "en";
+        case "en":
+          return "ar";
+        case "ar":
+          return "fr";
+        default:
+          return "fr";
+      }
+    });
+  };
+  const t = translations[language];
   return (
-    <div className="flex h-screen bg-gray-400">
+    <div
+      className={`flex h-screen ${
+        isDarkMode ? "bg-gray-900 text-white" : "bg-gray-400 text-black"
+      } ${language === "ar" ? "rtl" : "ltr"}`}
+    >
       {/* Navigation Sidebar */}
       <div
-        className={`${isNavOpen ? "w-64" : "w-20"} bg-white
- text-black transition-all duration-300 ease-in-out relative z-50`}
+        className={`${isNavOpen ? "w-64" : "w-20"} ${
+          isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+        }  transition-all duration-300 ease-in-out relative z-50`}
       >
         <button
           onClick={() => setIsNavOpen(!isNavOpen)}
-          className="absolute -right-3 top-9 bg-gray-200
-           rounded-full p-1 z-10"
+          className={`absolute -right-3 top-9 ${
+            isDarkMode ? "bg-black" : "bg-gray-200"
+          }
+           rounded-full p-1 z-10`}
         >
           {isNavOpen ? <ChevronLeft size={20} /> : <ChevronRight size={20} />}
         </button>
@@ -96,33 +132,49 @@ function App() {
           <div className="flex items-center mb-8">
             <Menu className="h-8 w-8" />
             {isNavOpen && (
-              <span className="ml-2 text-xl font-bold">
-                Panneau de Contrôle
-              </span>
+              <span className="ml-2 text-xl font-bold">{t.controlPanel}</span>
             )}
           </div>
 
           <nav>
             <NavItem
               icon={<Home />}
-              text="Accueil"
+              text={t.home}
               isOpen={isNavOpen}
               onClick={() => setCurrentPage("home")}
             />
             <NavItem
               icon={<Map />}
-              text="Carte"
+              text={t.map}
               isOpen={isNavOpen}
               onClick={() => setCurrentPage("map")}
             />
-            <NavItem icon={<Users />} text="Utilisateurs" isOpen={isNavOpen} />
+            <NavItem icon={<Users />} text={t.users} isOpen={isNavOpen} />
             <NavItem
               icon={<MessageSquare />}
-              text="Messages"
+              text={t.messages}
               isOpen={isNavOpen}
             />
-            <NavItem icon={<Settings />} text="Paramètres" isOpen={isNavOpen} />
+            <NavItem icon={<Settings />} text={t.settings} isOpen={isNavOpen} />
           </nav>
+          <button
+            onClick={toggleDarkMode}
+            className={`absolute -right-3 bottom-1/3 p-2 rounded-full ${
+              isDarkMode ? "bg-black text-white" : "bg-gray-200 text-black"
+            }`}
+            aria-label="Toggle dark mode"
+          >
+            {isDarkMode ? <Sun size={20} /> : <Moon size={20} />}
+          </button>
+          <button
+            onClick={changeLanguage}
+            className={`absolute -right-3 top-1/2 p-2 rounded-full ${
+              isDarkMode ? "bg-black text-white" : "bg-gray-200 text-black"
+            }`}
+            aria-label="Change language"
+          >
+            <Globe size={20} />
+          </button>
         </div>
       </div>
 
@@ -131,13 +183,23 @@ function App() {
         {/* Title Bar */}
         {currentPage === "map" ? (
           <div
-            className="bg-gradient-to-r from-white via-gray-200 to-white
- shadow-lg p-6 rounded-b-2xl border-b-4 border-blue-700 flex items-center justify-center gap-3"
+            className={`${
+              isDarkMode
+                ? "bg-gradient-to-r from-gray-800 via-black to-gray-800"
+                : "bg-gradient-to-r from-white via-gray-200 to-white"
+            } 
+ shadow-lg p-6 rounded-b-2xl border-b-4 border-blue-700 flex items-center justify-center gap-3`}
           >
-            <Map size={32} className="text-blue-700" />
-            <h1 className="text-3xl font-extrabold text-black drop-shadow-lg">
-              Conception d'un SIG foncier pour le plan régional d'habitat à
-              l'horizon 2034
+            <Map
+              size={32}
+              className={`${isDarkMode ? "text-blue-300" : "text-blue-700"}`}
+            />
+            <h1
+              className={`text-3xl font-extrabold ${
+                isDarkMode ? "text-white" : "text-black"
+              } drop-shadow-lg`}
+            >
+              {t.title}
             </h1>
           </div>
         ) : null}
@@ -178,16 +240,50 @@ function App() {
                 )}
               </MapContainer>
               {/* Légende */}
-              <Legend />
+              <div
+                className={`absolute bottom-6 right-6 ${
+                  isDarkMode ? "bg-gray-900" : "bg-white"
+                }
+ shadow-lg rounded-lg p-4 border border-gray-300`}
+              >
+                <h3
+                  className={`font-bold ${
+                    isDarkMode ? "text-blue-300" : "text-blue-700"
+                  } mb-3 text-lg`}
+                >
+                  {t.legend}
+                </h3>
+                <ul>
+                  {polygons.map((polygon, index) => (
+                    <li key={index} className="flex items-center mb-2">
+                      <span
+                        className="w-4 h-4 inline-block rounded-full mr-2"
+                        style={{ backgroundColor: polygon.color }}
+                      ></span>
+                      <span
+                        className={`${
+                          isDarkMode ? "text-white" : "text-black"
+                        }`}
+                      >
+                        {t[polygon.name]}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
               {/* Fenêtre de Contrôle des Couches */}
               <div
                 className={`absolute bottom-8 transition-all duration-300 ease-in-out ${
                   isNavOpen ? "left-72" : "left-24"
-                } w-56 bg-white
+                } w-56 ${isDarkMode ? "bg-gray-900" : "bg-white"}
  p-4 rounded-lg shadow-lg z-10`}
               >
-                <h3 className="font-bold text-blue-700 text-lg mb-4">
-                  Sélectionnez une Couche
+                <h3
+                  className={`font-bold ${
+                    isDarkMode ? "text-blue-300" : "text-blue-700"
+                  } text-lg mb-4`}
+                >
+                  {t.selectLayer}
                 </h3>
                 <div className="flex flex-col space-y-3">
                   <div className="flex items-center">
@@ -197,8 +293,10 @@ function App() {
                       onChange={() => toggleLayer("couche1")}
                       className="mr-2"
                     />
-                    <label className="text-black">
-                      Couche 1 (Zone Résidentielle A et Zone Écologique)
+                    <label
+                      className={`${isDarkMode ? "text-white" : "text-black"}`}
+                    >
+                      {t.layer1}
                     </label>
                   </div>
                   <div className="flex items-center">
@@ -208,37 +306,20 @@ function App() {
                       onChange={() => toggleLayer("couche2")}
                       className="mr-2"
                     />
-                    <label className="text-black">Couche 2 (Zone Mixte)</label>
+                    <label
+                      className={`${isDarkMode ? "text-white" : "text-black"}`}
+                    >
+                      {t.layer2}
+                    </label>
                   </div>
                 </div>
               </div>
             </>
           ) : (
-            <h2 className="text-4xl font-bold text-blue-900">Hello 👋</h2>
+            <h2 className="text-4xl font-bold text-blue-900">{t.hello}</h2>
           )}
         </div>
       </div>
-    </div>
-  );
-}
-function Legend() {
-  return (
-    <div
-      className="absolute bottom-6 right-6 bg-white
- shadow-lg rounded-lg p-4 border border-gray-300"
-    >
-      <h3 className="font-bold text-blue-700 mb-3 text-lg">Légende</h3>
-      <ul>
-        {polygons.map((polygon, index) => (
-          <li key={index} className="flex items-center mb-2">
-            <span
-              className="w-4 h-4 inline-block rounded-full mr-2"
-              style={{ backgroundColor: polygon.color }}
-            ></span>
-            <span className="text-black">{polygon.name}</span>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
